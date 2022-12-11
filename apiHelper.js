2cbc9d171273ee46ce9839340b511976c6a353c6
Fredrick Gichuhi <fredgkimani@gmail.com>
2022-12-11 09:46:10 +0300

    stocks path added

---
+++ apiHelper.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+module.exports = {
+  /*
+   ** This method returns a promise
+   ** which gets resolved or rejected based
+   ** on the result from the API
+   */
+  make_API_call: function (url) {
+    return new Promise((resolve, reject) => {
+      request(url, { json: true }, (err, res, body) => {
+        if (err) reject(err);
+        resolve(body);
+      });
+    });
+  },
+};
+
+// All symbols
+// https://cloud.iexapis.com/beta/ref-data/symbols?token=pk_77ef1294a9fd4bfeb1523b52629328e1
+
+// const symbols = ["AAPL", "META", "GOOGL", "TSLA"];
+// `https://cloud.iexapis.com/v1/stock/market/batch?symbols=${tickerArray
+//   .toString()
+//   .toLowerCase()}&types=quote&token=${process.env.IEXCLOUD_API_KEY}`;
+
+// batch mutliple symbols
+// https://cloud.iexapis.com/v1/stock/market/batch?&types=quote&symbols=aapl,fb,tsla&token=pk_77ef1294a9fd4bfeb1523b52629328e1
--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const api_helper = require("./apiHelper");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+
+app.use(cors());
 const PORT = 4000;
+const api_key = process.env.PK_KEY;
+const baseUrl = `https://cloud.iexapis.com/`;
+const stocksUrl = `https://cloud.iexapis.com/stable/ref-data/symbols/?token=pk_77ef1294a9fd4bfeb1523b52629328e1`;
 
-app.get("/", (req, res) => {
-  res.send("hello from backend");
-});
+const symbols = ["AAPL", "META", "GOOGL", "TSLA"];
+const quotes = `https://cloud.iexapis.com/v1/stock/market/batch?symbols=${symbols
+  .toString()
+  .toLowerCase()}&types=quote&token=${api_key}`;
 
-app.post("/user", (req, res) => {
-  res.send("Got a post request at /user");
+app.use(express.json()); // for parsing application/json
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/stocks", cors(), (req, res) => {
+  api_helper
+    .make_API_call(`${quotes}`)
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 app.listen(PORT, (err) => {
