93fcea9229a4eda0a77d84e2c3ceb7a897ad8476
Fredrick Gichuhi <fredgkimani@gmail.com>
2022-12-27 20:18:36 +0300

    fx historical data endpoints changed

--- server.js
+++ server.js
@@ -15,16 +15,21 @@ const PORT = process.env.PORT;
 const API_KEY = process.env.PK_KEY;
 const FH_KEY = process.env.FH_KEY;
 const AV_KEY = process.env.AV_KEY;
+const FCS_KEY = process.env.FCS_KEY;
+console.log(FCS_KEY);
 
-const api_key = finnhub.ApiClient.instance.authentications["api_key"];
-api_key.apiKey = FH_KEY;
+// const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+// api_key.apiKey = FH_KEY;
 
 const apiPath = `https://cloud.iexapis.com/v1/stock/market/batch?&types=quote&symbols=aapl,fb,tsla?token=${API_KEY}`;
 const history = `https://cloud.iexapis.com/v1/stock/AAPL/chart/1m?token=${API_KEY}`;
+// symbols
 const urlForex = `https://finnhub.io/api/v1/forex/symbol?exchange=oanda&token=cegligiad3i0qis37ar0cegligiad3i0qis37arg`;
 const fxDaily = `https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=${AV_KEY}`;
 const cryptoCurrency = `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=${AV_KEY}`;
-const newsUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=COIN,CRYPTO:BTC,FOREX:USD&time_from=20220410T0130&limit=200&apikey=AV_KEY`;
+const newsUrl = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=COIN,CRYPTO:BTC,FOREX:USD&time_from=20220410T0130&limit=200&?apikey=AV_KEY`;
+// Currencies
+const forexSymbols = `https://fcsapi.com/api-v3/forex/latest?symbol=all_forex&access_key=${FCS_KEY}`;
 
 async function getData(url) {
   try {
@@ -75,12 +80,16 @@ app.get("/stocks", async (req, res) => {
 });
 
 app.get("/forex", async (req, res) => {
-  const finnhubClient = new finnhub.DefaultApi();
+  const data = await fetchSymbols(
+    "https://fcsapi.com/api-v3/forex/history?symbol=EUR/USD&period=1h&access_key=ww22XgaGZAg1DsvPXsqRBa6N"
+  );
+  // const finnhubClient = new finnhub.DefaultApi();
 
-  finnhubClient.forexSymbols("OANDA", (error, data, response) => {
-    const fxCurrency = getDisplaySymbol(data);
-    res.status(200).send(fxCurrency);
-  });
+  // finnhubClient.forexSymbols("OANDA", (error, data, response) => {
+  //   const fxCurrency = getDisplaySymbol(data);
+
+  res.status(200).send(data);
+  // });
 });
 
 // Forex Daily time series
